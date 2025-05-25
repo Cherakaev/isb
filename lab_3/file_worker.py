6e52765c3853cf96dba4db7165f7c4365d008da8
Cherakaev <cherakaevv.yarik@gmail.com>
2025-05-26 03:07:09 +0400

    added encryption and decryption

---
+++ lab_3/file_worker.py
@@ -0,0 +1,146 @@
+import json
+
+from typing import Dict
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric.rsa import (
+    RSAPublicKey,
+    RSAPrivateKey
+)
+
+
+class FileWorker:
+    @staticmethod
+    def read_json(json_path: str) -> Dict[str, str]:
+        """
+        Method read json file as a dict with requirements
+        :param json_path: path of file.json
+        :return: dictionary
+        """
+        try:
+            with open(json_path, 'r', encoding='utf-8') as f:
+                return json.load(f)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"JSON file not found: {json_path}")
+        except json.JSONDecodeError:
+            raise ValueError(f"Invalid JSON format in file: {json_path}")
+
+    @staticmethod
+    def read_txt(txt_path: str) -> bytes:
+        """
+        Method reads file as a string
+        :param txt_path: path of file.txt
+        :return: text as str
+        """
+        try:
+            with open(txt_path, 'rb') as f:
+                return f.read()
+        except FileNotFoundError:
+            raise FileNotFoundError(f"File not found: {txt_path}")
+
+    @staticmethod
+    def write_txt(file_path: str, text: bytes) -> None:
+        """
+        Writes down text to the file
+        :param file_path: file path to write
+        :param text: text to write
+        :return: None
+        """
+        try:
+            with open(file_path, 'wb') as file:
+                file.write(text)
+        except Exception as e:
+            raise Exception(f"Error: {str(e)}")
+
+    @staticmethod
+    def serialize_public_key(public_key: RSAPublicKey,
+                             public_key_path: str) -> None:
+        """
+        Method serializes rsa public key to .pem file
+        :param public_key: key to serialize
+        :param public_key_path: path to file to save key
+        :return: None
+        """
+        if not public_key:
+            raise ValueError("Keys are not provided")
+
+        with open(public_key_path, "wb") as f:
+            f.write(
+                public_key.public_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PublicFormat.SubjectPublicKeyInfo,
+                )
+            )
+
+    @staticmethod
+    def serialize_private_key(private_key: RSAPrivateKey,
+                              private_key_path: str) -> None:
+        """
+        Method serialize rsa private key to .pem file
+        :param private_key: key to serialize
+        :param private_key_path: path to file to save key
+        :return: None
+        """
+        if not private_key:
+            raise ValueError("Key is not provided")
+        try :
+            with open(private_key_path, "wb") as f:
+                f.write(
+                    private_key.private_bytes(
+                        encoding=serialization.Encoding.PEM,
+                        format=serialization.PrivateFormat.PKCS8,
+                        encryption_algorithm=serialization.NoEncryption(),
+                    )
+                )
+        except Exception as e:
+            raise IOError(f"Failed to serialize private key: {str(e)}")
+
+    @staticmethod
+    def deserialize_private_key(private_key_path: str) -> RSAPrivateKey:
+        """
+        Method deserialize rsa private key from .pem file
+        :param private_key_path: path to file with key
+        :return: private key as class RSA
+        """
+        if not private_key_path:
+            raise ValueError("Key file path cannot be empty")
+
+        try:
+            with open(private_key_path, 'rb') as f:
+                private_key = serialization.load_pem_private_key(
+                    f.read(),
+                    password=None
+                )
+
+            if not isinstance(private_key, RSAPrivateKey):
+                raise ValueError("The file does not contain a valid "
+                                 "RSA private key")
+
+            return private_key
+
+        except Exception as e:
+            raise IOError(f"Failed to deserialize private key: {str(e)}")
+
+    @staticmethod
+    def deserialize_public_key(public_key_path: str) -> RSAPublicKey:
+        """
+        Method deserialize rsa public key from .pem file
+        :param public_key_path: path to file with key
+        :return: public key as class RSA
+        """
+        if not public_key_path:
+            raise ValueError("Key file path cannot be empty")
+
+        try:
+            with open(public_key_path, 'rb') as f:
+                public_key = serialization.load_pem_public_key(
+                    f.read(),
+                )
+
+            if not isinstance(public_key, RSAPublicKey):
+                raise ValueError("The file does not contain a valid "
+                                 "RSA public key")
+
+            return public_key
+
+        except Exception as e:
+            raise IOError(f"Failed to deserialize public key: {str(e)}")
---
+++ lab_3/hybrid_system.py
@@ -0,0 +1,88 @@
+import os
+
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import hashes, padding as sym_padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
+from file_worker import FileWorker
+
+
+class HybridSystem:
+    @staticmethod
+    def decrypt_symmetric_key(encrypted_key_path: str,
+                          private_key_path: str) -> bytes:
+        """
+        Reads and decrypts symmetric key
+        :param encrypted_key_path: path to file with key
+        :param private_key_path: path to file with private key
+        :return: decrypted symmetric key
+        """
+        encrypted_symmetric_key = FileWorker.read_txt(encrypted_key_path)
+        private_key = FileWorker.deserialize_private_key(private_key_path)
+
+        symmetric_key = private_key.decrypt(
+            encrypted_symmetric_key,
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None,
+            )
+        )
+        return symmetric_key
+
+    @staticmethod
+    def encrypt_file(initial_file_path: str,
+                     symmetric_key: bytes,
+                     encrypted_file_path: str) -> None:
+        """
+        Method encrypts file using RSA and symmetric key
+        :param initial_file_path: file path to encrypt
+        :param symmetric_key: already decrypted key
+        :param encrypted_file_path: file path to save result
+        :return: None
+        """
+        plaintext = FileWorker.read_txt(initial_file_path)
+
+        iv = os.urandom(16)
+        cipher = Cipher(algorithms.Camellia(symmetric_key),
+                        modes.CBC(iv),
+                        backend=default_backend())
+        encryptor = cipher.encryptor()
+
+        padder = sym_padding.PKCS7(128).padder()
+        padded_data = padder.update(plaintext) + padder.finalize()
+
+        ciphertext = encryptor.update(padded_data) + encryptor.finalize()
+
+        FileWorker.write_txt(encrypted_file_path, iv+ciphertext)
+
+    @staticmethod
+    def decrypt_file(encrypted_file_path: str, symmetric_key: bytes,
+                     decrypted_file_path: str) -> None:
+        """
+        Method encrypts file using RSA and symmetric key
+        :param encrypted_file_path: file path with encrypted text
+        :param symmetric_key: already decrypted key
+        :param decrypted_file_path: file path to save result
+        :return: None
+        """
+        data = FileWorker.read_txt(encrypted_file_path)
+
+        iv = data[:16]
+        ciphertext = data[16:]
+
+        cipher = Cipher(
+            algorithms.Camellia(symmetric_key),
+            modes.CBC(iv),
+            backend=default_backend()
+        )
+        decryptor = cipher.decryptor()
+
+        decrypted_padded = decryptor.update(ciphertext) + decryptor.finalize()
+
+        unpadder = sym_padding.PKCS7(128).unpadder()
+        decrypted_data = (unpadder.update(decrypted_padded)
+                          + unpadder.finalize())
+
+        FileWorker.write_txt(decrypted_file_path, decrypted_data)
